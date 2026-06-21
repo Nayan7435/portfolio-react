f846780d1dc75adffd9b00c062b8f42beb18c5cf
Gohel Nayan <gohelnayan264-gmail@gmail.com>
2026-06-21 15:32:06 +0530

    Initial portfolio commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <h1 className="text-4xl font-bold text-blue-600 text-center mt-10">
+      Tailwind is working!
+    </h1>
+  )
+}
+
+export default App
